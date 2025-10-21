88f6f6bb1fc921ed491ce2faabafea179b9e0ed0
Arunkumar Sanku <115217997+arunkumarsanku@users.noreply.github.com>
2025-10-21 20:28:44 +0200

    Time: 0 ms (100.00%) | Memory: 42 MB (49.01%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,18 +1,26 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0, count1 = 0, count2 = 0;
+        int low = 0, mid = 0, high = nums.length - 1;
 
-        // Count number of 0s, 1s and 2s
-        for (int num : nums) {
-            if (num == 0) count0++;
-            else if (num == 1) count1++;
-            else count2++;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // swap nums[low] and nums[mid]
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else { // nums[mid] == 2
+                // swap nums[mid] and nums[high]
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
         }
-
-        // Overwrite array
-        int index = 0;
-        while (count0-- > 0) nums[index++] = 0;
-        while (count1-- > 0) nums[index++] = 1;
-        while (count2-- > 0) nums[index++] = 2;
     }
 }
