1798554157791e89102eb0f0a11d67389ee004d7
Arunkumar Sanku <115217997+arunkumarsanku@users.noreply.github.com>
2025-10-26 11:12:11 +0100

    Time: 102 ms (30.12%) | Memory: 101.5 MB (29.76%) - LeetSync

---
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -0,0 +1,31 @@
+class Bank {
+    private long[] balance;
+
+    public Bank(long[] balance) {
+        this.balance = balance;
+    }
+
+    private boolean valid(int acc) {
+        return acc > 0 && acc <= balance.length;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (!valid(account1) || !valid(account2)) return false;
+        if (balance[account1 - 1] < money) return false;
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (!valid(account)) return false;
+        balance[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (!valid(account) || balance[account - 1] < money) return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+}
